a6617e84fac12a8f5f425e5ebbe7f76faa65d051
Ing. Miguel Angel Jiménez S., MA <66656638+oracle-dba-jimenez@users.noreply.github.com>
2020-06-09 01:47:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// Clase-1 6/jun/2020
+
+        //const prueba = 'Prueba de variable constante';
+        console.log (prueba);
+    
+        console.log ("Hola Miguel");
+    
+        let a;
+        console.log ('Tipo de dato de: a', typeof a);
+    
+        let b = 2;
+        console.log ('Tipo de dato de : b', typeof b);
+    
+        let c = "";
+        console.log ('Tipo de dato de : c', typeof c);
+    
+        let d = new Date();
+        console.log ('Tipo de dato de : d', typeof d);
+    
+        let e = true;
+        console.log ('Tipo de dato de : e', typeof e);
+    
+        let f = false;
+        console.log ('Tipo de dato de : f', typeof f);
+    
+        let g = 2.2;
+        console.log ('Tipo de dato de :g', typeof g);
+    
+
